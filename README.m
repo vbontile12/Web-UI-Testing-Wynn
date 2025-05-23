c16198ca84265fd7ef08647398065b5de95be083
vbontile12 <140018912+vbontile12@users.noreply.github.com>
2025-05-23 22:32:23 +0200

    Update README.m

--- README.m
+++ README.m
@@ -1,22 +1,12 @@
-# Playwright Drag-and-Drop File Upload Test (Dropzone.js)
+# 🧪 Playwright Drag-and-Drop File Upload Test (Dropzone.js)
 
-This project automates file uploads using Playwright and simulates drag-and-drop interactions on a Dropzone.js-based UI using the Page Object Model (POM) structure.
+This project automates file uploads using **Playwright** and simulates drag-and-drop interactions with a **Dropzone.js** interface, using the **Page Object Model (POM)** structure.
 
----
-
-
-## Setup Instructions
-
-### 1. Clone the repository
+## ⚙️ Setup Instructions
 
 ```bash
 git clone https://github.com/vbontile12/Web-UI-Testing-Wynn.git
 cd Web-UI-Testing-Wynn
-
-to install node modules run 
 npm install
-
-To run test use 
+npx playwright install
 npm test
-
-
